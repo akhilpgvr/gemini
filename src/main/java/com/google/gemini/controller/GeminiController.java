ae392a134e48a01e86c2226fb132cd9abe345255
akhilpgvr <akhilpgvr@gmail.com>
2024-09-26 01:48:50 +0530

    added CrossOrigin annotation to api -- for UI connection

--- src/main/java/com/google/gemini/controller/GeminiController.java
+++ src/main/java/com/google/gemini/controller/GeminiController.java
@@ -9,10 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -23,6 +20,7 @@ public class GeminiController implements GeminiInterface {
     @Autowired
     private GeminiService geminiService;
 
+    @CrossOrigin
     @Override
     @Operation(summary = "test api", description = "")
     @PostMapping("/ask-alpha")
